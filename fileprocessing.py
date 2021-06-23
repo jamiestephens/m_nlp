05fa6e30f605b48fb0ff1e140ea2eedd9b8c94ad
Jamie Stephens <71529189+jstephens@users.noreply.github.com>
2021-06-23 03:47:54 -0400

    updates

--- fileprocessing.py
+++ fileprocessing.py
@@ -13,7 +13,10 @@ from nltk.stem import WordNetLemmatizer
 nltk.download('wordnet')
 from nltk.corpus import stopwords
 from textdistance import levenshtein
- 
+from sklearn.feature_extraction.text import TfidfVectorizer
+import pickle
+import numpy as np 
+
 w_tokenizer = nltk.tokenize.WhitespaceTokenizer()
 lemmatizer = nltk.stem.WordNetLemmatizer()
 
@@ -57,15 +60,28 @@ def cleaningthefile():
 def addtlcleaning():
     df = pd.read_csv('./data/processed_data_prelim.csv')
     
-    df.rating.value_counts(normalize=True)
-   # df = df[df.rating!=0]
-   # df = df[df.rating!=1]
-   # df = df[df.rating!=2]  
+    df = df[df.rating!=0]
+    df = df[df.rating!=1]
+    df = df[df.rating!=2]  
+    
+    df.review= df.review.str.replace('      ',' ',regex=True)    
+    df.review= df.review.str.replace('     ',' ',regex=True) 
+    df.review= df.review.str.replace('    ',' ',regex=True) 
+    df.review= df.review.str.replace('   ',' ',regex=True) 
+    df.review= df.review.str.replace('  ',' ',regex=True) 
+    df = df[df['review'].str.len() >= 10]  
     
     print(df.head())
     print(len(pd.unique(df['URL'])))
     df.review= df.review.str.replace('Like   see reviewnannannan',' ',regex=True)    
     df.review= df.review.str.replace('like   see reviewnannannan',' ',regex=True)
+    df.review= df.review.str.replace('worldcrafting','worldbuild',regex=True)
+    df.review= df.review.str.replace('worldbuilding','worldbuild',regex=True)
+    df.review= df.review.str.replace('world building','worldbuild',regex=True)
+    df.review= df.review.str.replace('world-building','worldbuild',regex=True)
+    
+    
+    df.review= df.review.str.replace('Recommends it for ',' ',regex=True)
     df.review= df.review.str.replace('ya','young adult',regex=True)    
     alphanumeric = lambda x: re.sub('\w*\d\w*', ' ', x)
     df.review= df.review.str.replace('This review has been hidden because it contains spoilers. To view it click here.','',regex=True)      
@@ -73,133 +89,69 @@ def addtlcleaning():
     df.review= df.review.str.replace('time-travel','time travel',regex=True)  
     df.review= df.review.str.replace('dystopic','dystopia',regex=True)  
     df.review= df.review.str.replace('dystopian','dystopia',regex=True) 
-
-
-    
+    df.review= df.review.str.replace('dystopians','dystopia',regex=True)     
+ 
     df.apply(lambda x: levenshtein.distance(x['review'],  x['review']), axis=1)
    
     
-    df.review= df.review.str.replace('young adult','youngadult',regex=True)    
+    df.review= df.review.str.replace('youngadult','young adult',regex=True)    
     punc_lower = lambda x: re.sub('[%s]' % re.escape(string.punctuation), ' ', x.lower())
     df['review'] = df.review.map(alphanumeric).map(punc_lower) 
     
-    df.review= df.review.str.replace(' havent ',' ',regex=True)
-    df.review= df.review.str.replace(' dont ',' ',regex=True)
-    df.review= df.review.str.replace(' http ',' ',regex=True)
-    df.review= df.review.str.replace(' didnt ',' ',regex=True)
-    df.review= df.review.str.replace(' www ',' ',regex=True)
-    df.review= df.review.str.replace(' youtube ',' ',regex=True)
-    df.review= df.review.str.replace(' com ',' ',regex=True)
-    df.review= df.review.str.replace(' e ',' ',regex=True)
-    df.review= df.review.str.replace(' book ',' ',regex=True)   
-    df.review= df.review.str.replace(' wa ',' ',regex=True)   
-    df.review= df.review.str.replace(' arc ',' ',regex=True)    
-    df.review= df.review.str.replace(' (view spoiler) ',' ',regex=True)    
-    df.review= df.review.str.replace(' childrens ',' children ',regex=True)  
-    df.review= df.review.str.replace(' likenannannan ',' ',regex=True)
-    df.review= df.review.str.replace(' literature ','',regex=True)
-    df.review= df.review.str.replace(' reread ','',regex=True)
-    df.review= df.review.str.replace(' rereading ','',regex=True)
-    df.review= df.review.str.replace(' abridged ',' ',regex=True)
-    df.review= df.review.str.replace(' unabridged ',' ',regex=True)
-    df.review= df.review.str.replace(' movie ',' ',regex=True)
-    df.review= df.review.str.replace(' adapted ',' ',regex=True)
-    df.review= df.review.str.replace(' adaptation ',' ',regex=True)
-    df.review= df.review.str.replace(' sci-fi ',' ',regex=True)
-    df.review= df.review.str.replace(' scifi ',' ',regex=True)
-    df.review= df.review.str.replace(' sci ',' ',regex=True)
-    df.review= df.review.str.replace(' fi ',' ',regex=True)
-    df.review= df.review.str.replace(' sf ',' ',regex=True)
-    df.review= df.review.str.replace(' science fiction ',' ',regex=True)
-    df.review= df.review.str.replace(' science ',' ',regex=True)
-    df.review= df.review.str.replace(' spoiler',' ',regex=True)
-    df.review= df.review.str.replace(' fiction',' ',regex=True)
-    df.review= df.review.str.replace(' cover',' ',regex=True)
-    df.review= df.review.str.replace(' im ',' ',regex=True)
-    df.review= df.review.str.replace(' review ',' ',regex=True)
-    df.review= df.review.str.replace(' hugo ',' ',regex=True)
-    df.review= df.review.str.replace(' movie ',' ',regex=True)
-    df.review= df.review.str.replace(' ebook ',' ',regex=True)
-    df.review= df.review.str.replace(' e-book ',' ',regex=True)
-    df.review= df.review.str.replace(' kindle ',' ',regex=True)
-    df.review= df.review.str.replace(' sale ',' ',regex=True)
-    df.review= df.review.str.replace(' recommended ',' ',regex=True)
-    df.review= df.review.str.replace(' freebie ',' ',regex=True)
-    df.review= df.review.str.replace(' recommend ',' ',regex=True)
-    df.review= df.review.str.replace(' recommended ',' ',regex=True)
-    df.review= df.review.str.replace(' novel ',' ',regex=True)
-   
-    df.review= df.review.str.replace(' really ',' ',regex=True)
-    df.review= df.review.str.replace(' shelves ',' ',regex=True) 
-    df.review= df.review.str.replace(' read ',' ',regex=True)        
-    df.review= df.review.str.replace(' reading ',' ',regex=True)    
-    df.review= df.review.str.replace(' audio ',' ',regex=True)    
-    df.review= df.review.str.replace(' audiobook ',' ',regex=True)    
-    df.review= df.review.str.replace(' audiobooks ',' ',regex=True)    
-    df.review= df.review.str.replace(' author ',' ',regex=True)    
-    df.review= df.review.str.replace(' novel ',' ',regex=True)    
-    df.review= df.review.str.replace(' chapter ',' ',regex=True)   
-    df.review= df.review.str.replace(' nominee ',' ',regex=True)   
-    df.review= df.review.str.replace(' nebula ',' ',regex=True)   
-    df.review= df.review.str.replace(' story ',' ',regex=True)   
-    df.review= df.review.str.replace(' finalist ',' ',regex=True)  
-    df.review= df.review.str.replace(' recommendation ',' ',regex=True)   
-    df.review= df.review.str.replace(' ive ',' ',regex=True)        
-    df.review= df.review.str.replace(' page ',' ',regex=True)
-    df.review= df.review.str.replace(' pages',' ',regex=True)    
-    df.review= df.review.str.replace(' sample',' ',regex=True)       
-    df.review= df.review.str.replace(' used',' ',regex=True)    
-    df.review= df.review.str.replace(' copy',' ',regex=True)    
-    df.review= df.review.str.replace(' writer',' ',regex=True)    
-    df.review= df.review.str.replace(' novelist ',' ',regex=True)    
-    df.review= df.review.str.replace(' goodreads ',' ',regex=True)    
-    df.review= df.review.str.replace(' goodread ',' ',regex=True)    
-    df.review= df.review.str.replace(' shelf ',' ',regex=True)    
-    df.review= df.review.str.replace(' shelves ',' ',regex=True)    
-    df.review= df.review.str.replace(' star ',' ',regex=True)    
-    df.review= df.review.str.replace(' one ',' ',regex=True)    
-    df.review= df.review.str.replace(' two ',' ',regex=True)    
-    df.review= df.review.str.replace(' three ',' ',regex=True)    
-    df.review= df.review.str.replace(' four ',' ',regex=True)    
-    df.review= df.review.str.replace(' five ',' ',regex=True)    
-    df.review= df.review.str.replace(' recs ',' ',regex=True)    
-    df.review= df.review.str.replace(' favorite ',' ',regex=True)    
-    df.review= df.review.str.replace(' wont ',' ',regex=True)    
-    df.review= df.review.str.replace(' rate ',' ',regex=True)    
-    df.review= df.review.str.replace(' rated ',' ',regex=True)  
-    df.review= df.review.str.replace(' fav ',' ',regex=True)  
-    df.review= df.review.str.replace(' bookstore ',' ',regex=True)  
-    df.review= df.review.str.replace(' bought ',' ',regex=True)
-    df.review= df.review.str.replace(' dnfed ',' dnf ',regex=True)
-    df.review= df.review.str.replace(' rating ',' ',regex=True)
-    df.review= df.review.str.replace(' liked ',' ',regex=True)
-    df.review= df.review.str.replace(' originally ',' ',regex=True)
-    df.review= df.review.str.replace(' published ',' ',regex=True)
-    df.review= df.review.str.replace(' posted ',' ',regex=True)
-    df.review= df.review.str.replace(' reviewed ',' ',regex=True)
-    df.review= df.review.str.replace(' utopian ',' utopia ',regex=True)
-    df.review= df.review.str.replace(' finished ',' ',regex=True)
-    df.review= df.review.str.replace(' finish ',' ',regex=True)
-    #words = set(nltk.corpus.words.words())
+    df.review= df.review.str.replace('ha','',regex=True)
+
     
-    #text_translated = df.text.apply(words)
+    getrid = pd.read_csv('getrid.csv')
+    for index, row in getrid.iterrows():
+        phrase = " " + row['getridofit'] + " "
+        df.review= df.review.str.replace(phrase,' ',regex=True)
+   
+    df.review= df.review.str.replace(' childrens ',' children ',regex=True)   
+    
+    foreignwordsgetrid = pd.read_csv('foreigngetrid.csv')
+    for index, row in foreignwordsgetrid.iterrows():
+        phrase1 = " " + row['foreignword'] + " "
+        df.review= df.review.str.replace(phrase1,' ',regex=True)
+     
     df = df.reset_index(drop=True)
     
+    v = TfidfVectorizer(max_features=80)
+    x = v.fit_transform(df['review'])
+    print(v.get_feature_names())
+    
+    mostcommonwords = v.get_feature_names()
+    mostcommonwords.remove('time')
+    mostcommonwords.remove('world')
+    mostcommonwords.remove('first')
+    mostcommonwords.remove('good')
+    df["review"].replace(mostcommonwords, "", inplace=True)
+    
+    v1 = TfidfVectorizer(max_df = 1)
+    x1 = v1.fit_transform(df['review'])
+    leastcommonwords = v1.get_feature_names()
+    print(type(leastcommonwords))
+    
+    np.savetxt("leastcommonwordssub1.csv", 
+           leastcommonwords,
+           delimiter =", ", 
+           fmt ='% s')
+        
+    df.to_csv('./data/cleaned_data_for_rake.csv')
+
     df['review'] = df.review.apply(lemmatization)
+    
     stop = stopwords.words('english')
-   # stop.extend(new_stopwords)
     
     df['review'] = df['review'].apply(lambda x: [item for item in x if item not in stop])
     
-    
-    df[df['review'].apply(lambda x: len(x) > 10)]
-    df.to_csv('./data/processed_data_semifinal_ratings0-55.csv')
+
+    df.to_csv('./data/processed_data_final1.csv')
 
 def lemmatization(text):
     return [lemmatizer.lemmatize(w) for w in w_tokenizer.tokenize(text)]
 
 def concatenated():
-    df = pd.read_csv('./data/processed_data_final.csv')
+    df = pd.read_csv('./data/processed_data_prelim.csv')
     
     print(df.head())
     
@@ -218,9 +170,21 @@ def concatenated():
 
     print(grouped_lists.head())
     
-    grouped_lists.to_csv('./data/concatenatedreviews_noratings.csv')
+    grouped_lists.to_csv('./data/concatenatedreviews_new.csv')
 
 
+def concatenated_after_keywording():
+    df = pd.read_csv('./data/postrake.csv', index_col=[0, 1])
+   
+    df = df.groupby('URL')
+    
+    df = df['keywords'].agg(lambda column: " ".join(column))
+    
+    print(df.head())
+    
+    df = df.reset_index(name="keywords")
+    
+    df.to_csv('./data/keyworded_concatenated.csv')
 
 
 def edafile():
--- rake_and_cv.py
+++ rake_and_cv.py
@@ -16,37 +16,9 @@ from rake_nltk import Rake
 from sklearn.metrics.pairwise import cosine_similarity
 
 r = Rake() 
- 
-def supervisedl():
-    df = pd.read_csv('./data/output_ratings_rev.csv')
-    df.rating.value_counts(normalize=True)
-    df = df[df.rating!=0]
-    df = df[df.rating!=1]
-    df = df[df.rating!=2]    
-    
-    df['sentiment'] = np.where(df['rating'] >= 4, 'positive', 'negative')
-    
-    print(df.head())
-    
-    X = df.review
-    y = df.sentiment
-    
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
-    
-    tfidf1 = TfidfVectorizer(stop_words='english')
-    X_train_tfidf1 = tfidf1.fit_transform(X_train)
-    X_test_tfidf1  = tfidf1.transform(X_test)
-
-    tfidf2 = TfidfVectorizer(ngram_range=(1,2), binary=True, stop_words='english')
-    X_train_tfidf2 = tfidf2.fit_transform(X_train)
-    X_test_tfidf2  = tfidf2.transform(X_test)
-
-  #  lr.fit(X_train_tfidf1, y_train)
-  #  y_pred_tfidf1_lr = lr.predict(X_test_tfidf1)
-  #  cm5 = conf_matrix(y_test, y_pred_tfidf1_lr)
 
 def rake():
-    df = pd.read_csv('./data/concatenatedreviews_noratings.csv', index_col=[0, 1])
+    df = pd.read_csv('./data/cleaned_data_for_rake.csv', index_col=[0, 1])
         
     df['keywords'] = df['review'].apply(lambda x: rake_implement(x,r))
 
@@ -60,13 +32,23 @@ def rake_implement(x,r):
      return r.get_ranked_phrases()
 
 
-def countvectorizer():
-    df = pd.read_csv('./data/postrake.csv')
+def countvectorizer(title):
+    df = pd.read_csv('./data/postrake.csv', index_col=[0, 1])
     indices = pd.Series(df['URL'])
     count = CountVectorizer()
-    count_matrix = count.fit_transform(df['keywords'])
+    count_matrix = count.fit_transform(df['review'])
     cosine_sim = cosine_similarity(count_matrix, count_matrix)
     print(cosine_sim)
+    recommended_books = []
+    idx = indices[indices == title].index[0]
+    score_series = pd.Series(cosine_sim[idx]).sort_values(ascending = False)
+    top_10_indices = list(score_series.iloc[1:11].index)
+    
+    for i in top_10_indices:
+        recommended_books.append(list(df['URL'])[i])
+        
+    print(recommended_books)
+
 
 if __name__ == "__main__":
-    countvectorizer()
+    supervisedl()
